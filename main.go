593bfb5c2ac1e0d79333d6c08502fe1f4d4509eb
cgil <lao.tseu.is.alive@gmail.com>
2024-04-04 17:03:33 +0200

    working as wc

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"log"
 	"os"
+	"unicode"
 )
 
 func main() {
 	args := os.Args[1:]
 	if len(args) == 0 {
-		log.Fatalf("💥 ERROR: %s expects a filename as an argument", os.Args[0])
+		log.Fatalf("💥 ERROR: %s expects a text filename as first argument", os.Args[0])
 	}
 	filename := args[0]
 	log.Printf("📂 Will try to read file: %s", filename)
@@ -17,6 +18,25 @@ func main() {
 		log.Fatalf("💥 ERROR: trying to read file %s, error: %s", filename, err)
 	}
 	log.Printf("📂 File %s opened successfully", filename)
-	log.Printf("📂 File content: \n%s", content)
+	numBytes := len(content)
+	// count the number of lines and words in the content
+	numLines, numWords := 0, 0
+	inWord := false
+	for _, rune := range string(content) {
+		if rune == '\n' {
+			numLines++
+		}
+		if unicode.IsSpace(rune) {
+			if inWord {
+				inWord = false // Exiting a word
+			}
+		} else {
+			if !inWord {
+				numWords++
+				inWord = true // Entering a word
+			}
+		}
+	}
+	log.Printf("📂 File num lines, words and bytes: %d\t%d\t%d", numLines, numWords, numBytes)
 
 }
